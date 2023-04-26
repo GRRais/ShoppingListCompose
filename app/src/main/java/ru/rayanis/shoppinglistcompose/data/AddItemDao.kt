5ba4fa05b5c6e8966af4668e7b45ebe0086a813c
Rais Garifzyanov <garifzyanov.rr@gmail.com>
2023-04-26 16:26:17 +0300

    lesson 4 5.00

---
+++ app/src/main/java/ru/rayanis/shoppinglistcompose/data/AddItemDao.kt
@@ -0,0 +1,20 @@
+package ru.rayanis.shoppinglistcompose.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface AddItemDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertItem(item: AddItem)
+
+    @Delete
+    suspend fun deleteItem(item: AddItem)
+
+    @Query("SELECT * FROM add_item")
+    fun getAllItemsById(listId: Int): Flow<List<AddItem>>
+}
