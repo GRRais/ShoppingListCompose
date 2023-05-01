d7d78f70b6944a9e1769e93827323e2f677fe51b
Rais Garifzyanov <garifzyanov.rr@gmail.com>
2023-05-01 21:05:05 +0300

    lesson 12 15.00

--- app/src/main/java/ru/rayanis/shoppinglistcompose/shopping_list_screen/UiShoppingListItem.kt
+++ app/src/main/java/ru/rayanis/shoppinglistcompose/shopping_list_screen/UiShoppingListItem.kt
@@ -4,19 +4,27 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Card
+import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
+import ru.rayanis.shoppinglistcompose.ui.theme.DarkText
+import ru.rayanis.shoppinglistcompose.ui.theme.LightText
 
 @Preview(showBackground = true)
 @Composable
 fun UiShoppingListItem() {
-    ConstraintLayout(modifier = Modifier.padding(
-        start = 3.dp, top = 18.dp, end = 3.dp
-    )) {
+    ConstraintLayout(
+        modifier = Modifier.padding(
+            start = 3.dp, top = 18.dp, end = 3.dp
+        )
+    ) {
         val (card) = createRefs()
         Card(
             modifier = Modifier
@@ -27,10 +35,28 @@ fun UiShoppingListItem() {
                     end.linkTo(parent.end)
                 }
         ) {
-            Column(modifier = Modifier.fillMaxWidth()) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            ) {
                 Text(
                     text = "List 1",
-                    style =
+                    style = TextStyle(
+                        color = DarkText,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 16.sp
+                    )
+                )
+                Text(
+                    text = "12/12/2023",
+                    style = TextStyle(
+                        color = LightText,
+                        fontSize = 12.sp
+                    )
+                )
+                LinearProgressIndicator(
+                    modifier = Modifier.fillMaxWidth()
                 )
             }
         }
