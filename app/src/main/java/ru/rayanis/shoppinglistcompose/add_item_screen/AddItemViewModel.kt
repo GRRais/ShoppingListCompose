781428b44e8745ec72d99c2c931a22bd3c3cc06a
Rais Garifzyanov <garifzyanov.rr@gmail.com>
2023-05-07 19:46:23 +0300

    lesson 27 00.00

--- app/src/main/java/ru/rayanis/shoppinglistcompose/add_item_screen/AddItemViewModel.kt
+++ app/src/main/java/ru/rayanis/shoppinglistcompose/add_item_screen/AddItemViewModel.kt
@@ -1,34 +1,40 @@
 package ru.rayanis.shoppinglistcompose.add_item_screen
 
 import android.util.Log
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 import ru.rayanis.shoppinglistcompose.data.AddItem
 import ru.rayanis.shoppinglistcompose.data.AddItemRepository
 import ru.rayanis.shoppinglistcompose.dialog.DialogController
 import ru.rayanis.shoppinglistcompose.dialog.DialogEvent
-import ru.rayanis.shoppinglistcompose.main_screen.MainScreenEvent
 import javax.inject.Inject
 
 @HiltViewModel
 class AddItemViewModel @Inject constructor(
     private val repository: AddItemRepository,
     savedStateHandle: SavedStateHandle
-): ViewModel(), DialogController {
+) : ViewModel(), DialogController {
 
     var itemsList: Flow<List<AddItem>>? = null
 
+    var addItem: AddItem? = null
+    var listId: Int = -1
+
     init {
-        val listId = savedStateHandle.get<String>("listId")?.toInt()
+        listId = savedStateHandle.get<String>("listId")?.toInt()!!
         itemsList = listId?.let { repository.getAllItemsById(it) }
         Log.d("MyLog", "List id View model: $listId")
     }
 
-    override var dialogTitle = mutableStateOf("")
+    var itemText = mutableStateOf("")
+        private set
+
+    override var dialogTitle = mutableStateOf("Edit name:")
         private set
     override var editableText = mutableStateOf("")
         private set
@@ -37,8 +43,46 @@ class AddItemViewModel @Inject constructor(
     override var showEditableText = mutableStateOf(true)
         private set
 
-    fun onEvent() {
+    fun onEvent(event: AddItemEvent) {
+        when (event) {
+            is AddItemEvent.OnItemSave -> {
+                viewModelScope.launch {
+                    if (listId == -1) return@launch
+                    repository.insertItem(
+                        AddItem(
+                            addItem?.id,
+                            itemText.value,
+                            addItem?.isCheck ?: false,
+                            listId
+                        )
+                    )
+                    itemText.value = ""
+                    addItem = null
+                }
+            }
 
+            is AddItemEvent.OnShowEditDialog -> {
+                addItem = event.item
+                openDialog.value = true
+                editableText.value = addItem?.name ?: ""
+            }
+
+            is AddItemEvent.OnTextChange -> {
+                itemText.value = event.text
+            }
+
+            is AddItemEvent.OnDelete -> {
+                viewModelScope.launch {
+                    repository.deleteItem(event.item)
+                }
+            }
+
+            is AddItemEvent.OnCheckedChange -> {
+                viewModelScope.launch {
+                    repository.insertItem(event.item)
+                }
+            }
+        }
     }
 
     override fun onDialogEvent(event: DialogEvent) {
@@ -50,6 +94,7 @@ class AddItemViewModel @Inject constructor(
 
             is DialogEvent.OnConfirm -> {
                 openDialog.value = false
+                itemText.value = editableText.value
                 editableText.value = ""
             }
 
