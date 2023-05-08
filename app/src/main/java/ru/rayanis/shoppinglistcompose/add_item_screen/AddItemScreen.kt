c3d9beb1ac3f9ad60c221cec5319238d7c793014
Rais Garifzyanov <garifzyanov.rr@gmail.com>
2023-05-08 06:48:56 +0300

    lesson 29 00.00

--- app/src/main/java/ru/rayanis/shoppinglistcompose/add_item_screen/AddItemScreen.kt
+++ app/src/main/java/ru/rayanis/shoppinglistcompose/add_item_screen/AddItemScreen.kt
@@ -1,23 +1,114 @@
 package ru.rayanis.shoppinglistcompose.add_item_screen
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import ru.rayanis.shoppinglistcompose.R
 import ru.rayanis.shoppinglistcompose.data.AddItem
+import ru.rayanis.shoppinglistcompose.dialog.MainDialog
+import ru.rayanis.shoppinglistcompose.ui.theme.BlueLight
+import ru.rayanis.shoppinglistcompose.ui.theme.DarkText
+import ru.rayanis.shoppinglistcompose.ui.theme.GrayLight
 
+@Preview(showBackground = true)
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun AddItemScreen(
     viewModel: AddItemViewModel = hiltViewModel()
 ) {
-    Text(
-        text = "Add Item screen",
-        modifier = Modifier
-            .fillMaxSize()
-            .wrapContentHeight()
-            .wrapContentWidth()
-    )
+    val scaffoldState = rememberScaffoldState()
+    val itemsList = viewModel.itemsList?.collectAsState(initial = emptyList())
+
+    Scaffold(scaffoldState = scaffoldState) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(GrayLight)
+        ) {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(5.dp)
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    TextField(
+                        modifier = Modifier.weight(1f),
+                        value = "",
+                        onValueChange = {},
+                        label = {
+                            Text(
+                                text = "New item:",
+                                fontSize = 12.sp
+                            )
+                        },
+                        colors = TextFieldDefaults.textFieldColors(
+                            backgroundColor = Color.White,
+                            focusedIndicatorColor = BlueLight,
+                            unfocusedIndicatorColor = Color.Transparent
+                        ),
+                        textStyle = TextStyle(
+                            fontSize = 16.sp,
+                            color = DarkText
+                        ),
+                        singleLine = true
+                    )
+                    IconButton(
+                        onClick = {
+
+                        }
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.add_icon),
+                            contentDescription = "Add"
+                        )
+                    }
+                }
+            }
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = 10.dp)
+            ) {
+                if (itemsList != null) {
+                    items(itemsList.value) {item ->
+                        UiAddItem(item = item, onEvent = {
+
+                        })
+                    }
+                }
+            }
+        }
+        MainDialog(viewModel)
+    }
 }
