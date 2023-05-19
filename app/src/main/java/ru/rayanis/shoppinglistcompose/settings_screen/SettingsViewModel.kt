d38a89f0d79e7005a0fd514bbed811ac86894e8a
Rais Garifzyanov <garifzyanov.rr@gmail.com>
2023-05-19 05:40:34 +0300

    lesson 44 00.00

--- app/src/main/java/ru/rayanis/shoppinglistcompose/settings_screen/SettingsViewModel.kt
+++ app/src/main/java/ru/rayanis/shoppinglistcompose/settings_screen/SettingsViewModel.kt
@@ -5,16 +5,17 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
-class SettingsViewModel: ViewModel() {
+class SettingsViewModel @Inject constructor(): ViewModel() {
 
     val colorItemListState = mutableStateOf<List<ColorItem>>(emptyList())
 
     init {
         viewModelScope.launch {
             val tempColorItemList = ArrayList<ColorItem>()
-            ColorUtils.colorList.forEach {color ->
+            ColorUtils.colorList.forEach { color ->
                 tempColorItemList.add(
                     ColorItem(
                         color,
