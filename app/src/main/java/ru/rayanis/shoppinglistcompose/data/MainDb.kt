9c9772df57fe50cef711592ea1ed34f07ee1983c
Rais Garifzyanov <garifzyanov.rr@gmail.com>
2023-05-26 11:16:41 +0300

    final commit work version

--- app/src/main/java/ru/rayanis/shoppinglistcompose/data/MainDb.kt
+++ app/src/main/java/ru/rayanis/shoppinglistcompose/data/MainDb.kt
@@ -1,12 +1,7 @@
 package ru.rayanis.shoppinglistcompose.data
 
-import androidx.room.AutoMigration
 import androidx.room.Database
-import androidx.room.DeleteColumn
-import androidx.room.RenameColumn
-import androidx.room.RenameTable
 import androidx.room.RoomDatabase
-import androidx.room.migration.AutoMigrationSpec
 
 @Database(
     entities = [
@@ -15,25 +10,10 @@ import androidx.room.migration.AutoMigrationSpec
         NoteItem::class,
         TestItem::class
     ],
-    autoMigrations = [AutoMigration(
-        from = 6,
-        to = 7,
-        spec = MainDb.DeleteColumnFromShopList::class
-    )
-                     ],
-    version = 7,
+    version = 1,
     exportSchema = true
 )
 abstract class MainDb : RoomDatabase() {
-
-    @RenameColumn(
-        "shop_list_name",
-        fromColumnName = "time",
-        toColumnName = "my_time"
-    )
-
-    @DeleteColumn(tableName = "shop_list_name", columnName = "testCounter")
-    class DeleteColumnFromShopList: AutoMigrationSpec
 
     abstract val shoppingListDao: ShoppingListDao
     abstract val noteDao: NoteDao
